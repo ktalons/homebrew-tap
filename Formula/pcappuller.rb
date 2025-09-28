8b5abd2e1eb08f1218b87c6d4ef8808f2d7d00b6
kyle <ktalonsec@gmail.com>
2025-09-27 20:04:05 -0700

    brew: update pcappuller to v0.1.0 with correct SHA256

--- Formula/pcappuller.rb
+++ Formula/pcappuller.rb
@@ -3,7 +3,7 @@ class Pcappuller < Formula
   homepage "https://github.com/ktalons/daPCAPpuller"
   version "0.1.0"
   url "https://github.com/ktalons/daPCAPpuller/releases/download/v0.1.0/PCAPpullerGUI-macos"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "b8fa548603229e0984a84fb44326e4b046429b7470cd6192c7a38642d91dfb8f"
   license "MIT"
 
   def install
